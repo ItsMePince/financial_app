f032e2749ad41dd32f28b304e1e61e853e61e482
supawich6687099 <peeppee2@gmail.com>
2025-10-18 01:16:18 +0700

    update

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,11 +1,13 @@
-﻿import { defineConfig } from "cypress";
+﻿// cypress.config.ts
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    supportFile: "cypress/support/e2e.ts",
-  },
+    e2e: {
+        baseUrl: process.env.CYPRESS_baseUrl || "http://app.localtest.me:8080",
+        supportFile: "cypress/support/e2e.ts",
+        specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+        env: {
+            API_BASE: process.env.CYPRESS_API_URL || "/api",
+        },
+    },
 });
-
-
-
